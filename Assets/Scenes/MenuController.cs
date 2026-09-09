f18d7e2abc1600b8f200220c2ab698688b9f7e6a
momytelloffmealots <atflm.phu@gmail.com>
2026-09-09 11:35:15 +0700

    ok

---
+++ Assets/Scenes/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuController : MonoBehaviour
+{
+    // Chuyển sang Scene SampleScene
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Dừng chế độ Play trong Editor hoặc Thoát game khi đã Build
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false; // Thoát chế độ Play trong Unity Editor
+#else
+            Application.Quit(); // Thoát ứng dụng thật khi đã build
+#endif
+    }
+}
